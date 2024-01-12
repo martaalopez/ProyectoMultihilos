ce4cc59ae6970abac125597af18dd22f697bae31
Jose_Luis <joselu2004.jlrc@gmail.com>
2024-01-12 12:30:22 +0100

    pantalla login creada

--- src/main/java/com/example/proyectomultihilos/App.java
+++ src/main/java/com/example/proyectomultihilos/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("bookshelve"), 1000, 700);
+        scene = new Scene(loadFXML("login"), 1000, 700);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
--- src/main/java/com/example/proyectomultihilos/controller/LoginController.java
+++ src/main/java/com/example/proyectomultihilos/controller/LoginController.java
@@ -1,20 +1,41 @@
 package com.example.proyectomultihilos.controller;
+
 import com.example.proyectomultihilos.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
 public class LoginController {
 
     @FXML
+    private Label label;
+
+    @FXML
     private Button login;
 
     @FXML
-    public void login(ActionEvent event) throws IOException {
-        App.setRoot("bookshelve");
+    private PasswordField txtPassword;
 
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    void login(ActionEvent event) throws IOException {
+        if (txtUser.getText().equals("admin") && txtPassword.getText().equals("admin")){
+            label.setText("Correct user and password!");
+            label.setTextFill(Color.GREEN);
+            App.setRoot("bookshelve");
+        }else {
+            label.setText("Wrong username or password!");
+            label.setTextFill(Color.RED);
+        }
     }
+
 
 }
